e5f8fc0ecc4d82a9d15c8e01d016857e949309be
Min0522-sku <wkdalstj0522@gmail.com>
2026-02-26 10:52:38 +0900

    fix : 교수님 과 같이 풀이

--- src/main/java/example/day2/pratice2/RestController.java
+++ src/main/java/example/day2/pratice2/RestController.java
@@ -1,46 +1,83 @@
 package example.day2.pratice2;
 
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @org.springframework.web.bind.annotation.RestController
+@RequestMapping("/board")
 public class RestController {
-    private ArrayList<BoardDto> boardDto = new ArrayList<>();
-    @PostMapping("/board")
-    public boolean boardWrite(String bcontent, String bwriter){
-        boolean result = bcontent.isEmpty()&&bwriter.isEmpty();
-        return result;
-    }
-    @GetMapping("/board")
-    public ArrayList<BoardDto> boardPrint(){
-
-        return boardDto;
-    }
-    @GetMapping("/board/detail")
-    public BoardDto boardDetail(int bno){
-        return boardDto.get(bno);
-    }
-    @DeleteMapping("/board")
-    public boolean boardDelete(int bno){
-        boardDto.remove(bno);
+    @PostMapping
+    public boolean boardWrite( @RequestBody BoardDto boardDto ){ // @RequestParam vs @RequestBody
         return true;
     }
-    @PutMapping("/board")
-    public boolean boardUpdate(int bno, BoardDto bcontent){
-        boardDto.set(bno, bcontent);
+    @GetMapping
+    public ArrayList<BoardDto> boardPrint( ){
+        ArrayList<BoardDto> list = new ArrayList<>();
+        BoardDto boardDto1 = new BoardDto( 1 , "내용1" ,"작성자1");
+        BoardDto boardDto2 = new BoardDto( 2 , "내용2" ,"작성자2");
+        list.add( boardDto1 ); list.add( boardDto2 );
+        return list;
+    }
+    @GetMapping("/detail")
+    public BoardDto boardDetail( @RequestParam int bno ){
+        BoardDto boardDto = new BoardDto( 1 , "내용1" ,"작성자1");
+        return boardDto;
+    }
+    @DeleteMapping
+    public boolean boardDelete(  @RequestParam  int bno ){
+        return false;
+    }
+    @PutMapping
+    public boolean boardUpdate( @RequestBody BoardDto boardDto ){
         return true;
     }
 
 }
 
-class BoardDto{
+class BoardDto {
     private int bno;
     private String bcontent;
     private String bwriter;
 
+    public BoardDto(){}
+    public BoardDto(int bno, String bcontent, String bwriter) {
+        this.bno = bno;
+        this.bcontent = bcontent;
+        this.bwriter = bwriter;
+    }
+
+    public int getBno() {
+        return bno;
+    }
+
+    public void setBno(int bno) {
+        this.bno = bno;
+    }
+
+    public String getBcontent() {
+        return bcontent;
+    }
+
+    public void setBcontent(String bcontent) {
+        this.bcontent = bcontent;
+    }
+
+    public String getBwriter() {
+        return bwriter;
+    }
+
+    public void setBwriter(String bwriter) {
+        this.bwriter = bwriter;
+    }
+
+    @Override
+    public String toString() {
+        return "BoardDto{" +
+                "bno=" + bno +
+                ", bcontent='" + bcontent + '\'' +
+                ", bwriter='" + bwriter + '\'' +
+                '}';
+    }
 }
