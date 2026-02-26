630e8b0a4d80b6a05846c01d7c5011c8e9ad5c65
Min0522-sku <wkdalstj0522@gmail.com>
2026-02-26 10:14:35 +0900

    fix : 교수님 과 같이 풀이

--- src/main/java/example/day2/pratice2/PostController.java
+++ src/main/java/example/day2/pratice2/PostController.java
@@ -9,37 +9,38 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/pratice2/post")
 public class PostController {
-    private List<Map<String, String>> list = new ArrayList<>();
-    private Map<String, String> map1 = new HashMap<>();
-    public PostController() {
-        Map<String, String> map1 = new HashMap<>();
-        map1.put("pno", "1");
-        map1.put("ptitle", "제목1");
-        list.add(map1);
 
-        Map<String, String> map2 = new HashMap<>();
-        map2.put("pno", "2");
-        map2.put("ptitle", "제목2");
-        list.add(map2);
-    }
-    @PostMapping("/pratice2/post")
+    @PostMapping
     public boolean 글쓰기(){
         return true;
     }
-    @GetMapping("/pratice2/post")
-    public List<Map<String, String>> 전체글조회(){
+    @GetMapping
+    public List<Map<String, Object>> 전체글조회(){
+        List<Map<String, Object>> list = new ArrayList<>();
+        Map<String, Object> map1 = new HashMap<>();
+        map1.put("pno", "1");
+        map1.put("ptitle", "제목1");
+        list.add(map1);
+        Map<String, Object> map2 = new HashMap<>();
+        map2.put("pno", "2");
+        map2.put("ptitle", "제목2");
+        list.add(map2);
         return list;
     }
-    @GetMapping("/pratice2/post/view")
-    public Map<String, String> 개별글조회(){
+    @GetMapping("/view")
+    public Map<String, Object> 개별글조회(){
+        Map<String, Object> map1 = new HashMap<>();
+        map1.put("pno", "1");
+        map1.put("ptitle", "제목1");
         return map1;
     }
-    @PutMapping("/pratice2/post")
+    @PutMapping
     public boolean 개별글수정(){
         return true;
     }
-    @DeleteMapping("/pratice2/post")
+    @DeleteMapping
     public int 개별글삭제(){
         return 3;
     }
