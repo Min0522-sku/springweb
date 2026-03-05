3fb7a1e24fc263809434e6cbf638011767375ada
Min0522-sku <wkdalstj0522@gmail.com>
2026-03-05 13:03:04 +0900

    필요없는 import 삭제

--- src/main/java/example/day06/entity/BaseTime.java
+++ src/main/java/example/day06/entity/BaseTime.java
@@ -3,9 +3,7 @@ package example.day06.entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
