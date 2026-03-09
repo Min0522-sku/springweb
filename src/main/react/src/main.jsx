1b5d965e117b073097487924ead3376e9832baaa
Min0522-sku <wkdalstj0522@gmail.com>
2026-03-09 17:52:25 +0900

    react day01

--- src/main/react/src/main.jsx
+++ src/main/react/src/main.jsx
@@ -5,3 +5,9 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
     <App />
 )
+
+// // index.html(싱글페이지) 에서 root 라는  id 갖는 div 호출
+// const root = document.querySelector('#root');
+// // root 마크업에 렌더링(render)
+// createRoot(root).render(<h1>안녕하세요!</h1>)
+// // vs root.innerHTML = "<h1> 안녕하세요! </h1>";
