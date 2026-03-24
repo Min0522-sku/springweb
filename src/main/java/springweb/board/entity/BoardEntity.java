79ff3668b1038bc0ff7d260f97685c70b2d47bc9
Min0522-sku <wkdalstj0522@gmail.com>
2026-03-24 11:59:56 +0900

    day59 보드

---
+++ src/main/java/springweb/board/entity/BoardEntity.java
@@ -0,0 +1,28 @@
+package springweb.board.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import springweb.member.entity.MemberEntity;
+
+@Entity
+@Table(name = "board")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class BoardEntity extends BaseTime{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+    @Column(nullable = false, length = 255)
+    private String btitle;
+    @Column(nullable = false, columnDefinition = "longtext")
+    private String bcontent;
+    @Column(nullable = true, columnDefinition = "longtext") // 주로 첨부파일은 파일자체를 저장하는게 아니라 파일의 위치(서버내 경로) 저장
+    private String bfile;
+
+    // + 단방향 : 한명이 회원이 여러개 게시물 작성 1:N
+    @ManyToOne
+    @JoinColumn(name = "mno") @ToString.Exclude
+    private MemberEntity memberEntity;
+}
